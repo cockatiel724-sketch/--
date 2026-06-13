b54f901efd2c303f767256faa2486e6ffa84fa7d
cockatiel724-sketch <cockatiel724@gmail.com>
2026-06-13 10:42:26 +0900

    Update trail.js

--- systems/trail.js
+++ systems/trail.js
@@ -1,19 +1,44 @@
 const TrailSystem = {
 
-  create({ dx, dy }) {
+  create({
+    dx,
+    dy,
+    startX,
+    startY,
+    endX,
+    endY
+  }) {
 
     const trail = document.createElement("div");
 
     trail.className = "trail";
 
+    // スワイプ距離
+    const length = Math.sqrt(
+      dx * dx +
+      dy * dy
+    );
+
+    // 中間地点
+    const centerX =
+      (startX + endX) / 2;
+
+    const centerY =
+      (startY + endY) / 2;
+
+    trail.style.width =
+      `${length}px`;
+
     trail.style.left =
-      `${window.innerWidth / 2 - 60}px`;
+      `${centerX - length / 2}px`;
 
     trail.style.top =
-      `${window.innerHeight / 2 - 6}px`;
+      `${centerY - 6}px`;
 
     const angle =
-      Math.atan2(dy, dx) * 180 / Math.PI;
+      Math.atan2(dy, dx) *
+      180 /
+      Math.PI;
 
     trail.style.transform =
       `rotate(${angle}deg)`;
